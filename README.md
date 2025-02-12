0fa64118a01c88af23334150e1ea95d4795569c6
ChetanLingareddy <lingareddychetan@gmail.com>
2025-02-12 01:08:30 -0600

    Added email functionality, sending a topic , description and url of the news in the email

---
+++ README.md
@@ -0,0 +1,2 @@
+# What does this app do?
+Access news about a particular topic and send them by email.
--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import requests
 import os
+from Send_email import info
+topic = "tesla"
+api_key = os.getenv ( "API_KEY" )
+url = ("https://newsapi.org/v2/everything?"
+       f"q={topic}&"
+       "from=2025-01-12&"
+       "sortBy=publishedAt&apiKey=03e88b7b46114e15902f9e22433977f4&"
+       "language=en")
 
-api_key = os.getenv("API_KEY")
-url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=03e88b7b46114e15902f9e22433977f4"
-
-request = requests.get(url)
-content= request.json()
-
-for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
-    print(article["url"])
+request = requests.get ( url )
+content = request.json ()
+message = ""
+for article in content["articles"][:20] :
+    title = article["title"]
+    description = article["description"]
+    link = article["url"]
+    message += f"""
+TITLE:  {title}
+DESCRIPTION:  {description}
+URL LINK: {link}\n\n"""
+message=f"""Subject: Daily News
+{message}"""
+message = message.encode ( "utf-8" )
+info ( message )
